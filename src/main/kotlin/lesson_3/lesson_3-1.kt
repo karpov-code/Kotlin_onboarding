3ffd0df2ec951bf36ba020ea6582ff1cd05eb199
Карпов Андрей <andreyka527@yandex.ru>
2025-07-29 14:32:12 +0300

    KO-2-5

---
+++ src/main/kotlin/lesson_3/lesson_3-1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_3
+
+fun main(){
+    val username = "Ivan"
+    var greeting = "Good afternoon!"
+    println("$greeting $username")
+
+    greeting = "Good evening!"
+    println("$greeting $username")
+}
