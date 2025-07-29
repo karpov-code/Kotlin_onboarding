113f2b4ceebd962369d754beebbf6b6691d6398d
Карпов Андрей <andreyka527@yandex.ru>
2025-07-29 18:40:27 +0300

    KO-4-3

---
+++ src/main/kotlin/lesson_4/lesson_4-3.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_4
+
+fun main(){
+    val weatherIsSunny = true
+    val awningAtMoment = true
+    val airHumidity = 20
+    val season = "зима"
+
+    println("Благоприятные ли условия сейчас для роста бобовых?" +
+            " ${weatherIsSunny && awningAtMoment && airHumidity == 20 && season != "зима"}")
+}
