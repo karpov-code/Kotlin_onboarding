7eeea8db8347f834dbae8677a6dceba528537e7d
Карпов Андрей <andreyka527@yandex.ru>
2025-07-29 12:40:46 +0300

    KO-2-4

---
+++ src/main/kotlin/lesson_2/lesson_2-4.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_2
+
+fun main(){
+    val counterOfCrystalOre = 7
+    val counterOfIronOre = 11
+
+    print("Бонусная кристаллическая руда ")
+    println(counterOfCrystalOre*20/100)
+    print("Бонусная железная руда ")
+    print(counterOfIronOre*20/100)
+}
