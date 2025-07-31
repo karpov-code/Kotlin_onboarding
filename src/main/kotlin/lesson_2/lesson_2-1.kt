5873e909b9ba51f36f1042df7c620dbb46959bdc
Карпов Андрей <andreyka527@yandex.ru>
2025-07-31 12:14:04 +0300

    KS-2-1-adding a variable number of students

--- src/main/kotlin/lesson_2/lesson_2-1.kt
+++ src/main/kotlin/lesson_2/lesson_2-1.kt
@@ -1,7 +1,8 @@
 package org.example.lesson_2
 
 fun main() {
-    val sumForArithmeticMean = (3 + 4 + 3 + 5).toFloat()
+    val numberOfStudents = 3 + 4 + 3 + 5
+    val sumForArithmeticMean = numberOfStudents.toFloat()
     val arithmeticMean = sumForArithmeticMean / 4
     println(arithmeticMean)
 }
