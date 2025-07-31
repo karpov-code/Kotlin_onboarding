45b414b070fb836a09348910adacbce971fd78f7
Карпов Андрей <andreyka527@yandex.ru>
2025-07-31 22:45:00 +0300

    KS-3-4-formatting

--- src/main/kotlin/lesson_3/lesson_3-4.kt
+++ src/main/kotlin/lesson_3/lesson_3-4.kt
@@ -4,10 +4,10 @@ fun main() {
     var whereFrom = "E2"
     var where = "E4"
     var strokeNumber = 1
-    println("$whereFrom - $where, $strokeNumber")
+    println("$whereFrom-$where;$strokeNumber")
 
     whereFrom = "D2"
     where = "D3"
     strokeNumber += 1
-    println("$whereFrom - $where, $strokeNumber")
+    println("$whereFrom-$where;$strokeNumber")
 }
