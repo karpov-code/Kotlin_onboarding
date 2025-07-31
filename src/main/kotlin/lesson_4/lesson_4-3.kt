4d8ab37d6faf30e409578b046b63d2eb9bd11a2e
Карпов Андрей <andreyka527@yandex.ru>
2025-07-31 15:59:04 +0300

    KS-4-3-formatting

--- src/main/kotlin/lesson_4/lesson_4-3.kt
+++ src/main/kotlin/lesson_4/lesson_4-3.kt
@@ -1,11 +1,13 @@
 package org.example.lesson_4
 
-fun main(){
+fun main() {
     val weatherIsSunny = true
     val awningAtMoment = true
     val airHumidity = 20
     val season = "зима"
 
-    println("Благоприятные ли условия сейчас для роста бобовых?" +
-            " ${weatherIsSunny && awningAtMoment && airHumidity == 20 && season != "зима"}")
+    println(
+        "Благоприятные ли условия сейчас для роста бобовых?" +
+                " ${weatherIsSunny && awningAtMoment && airHumidity == 20 && season != "зима"}"
+    )
 }
