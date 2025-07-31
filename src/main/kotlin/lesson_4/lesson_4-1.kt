144b94b19e221c7d8a7f1eb76d6ccd2fd5db7a89
Карпов Андрей <andreyka527@yandex.ru>
2025-07-31 13:27:05 +0300

    KS-4-1-formatting

--- src/main/kotlin/lesson_4/lesson_4-1.kt
+++ src/main/kotlin/lesson_4/lesson_4-1.kt
@@ -2,10 +2,10 @@ package org.example.lesson_4
 
 const val NUMBER_OF_TABLES = 13
 
-fun main(){
-    val reservedtTablesToday = 13
+fun main() {
+    val reservedTablesToday = 13
     val reservedTablesTomorrow = 9
 
-    println("Доступность столиков на сегодня: ${reservedtTablesToday < NUMBER_OF_TABLES}")
+    println("Доступность столиков на сегодня: ${reservedTablesToday < NUMBER_OF_TABLES}")
     println("Доступность столиков на завтра: ${reservedTablesTomorrow < NUMBER_OF_TABLES}")
 }
