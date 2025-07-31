3c325510769297d8e0ecdf20f8dee95f0ff68891
Карпов Андрей <andreyka527@yandex.ru>
2025-07-31 16:00:39 +0300

    KS-4-4-formatting

--- src/main/kotlin/lesson_4/lesson_4-4.kt
+++ src/main/kotlin/lesson_4/lesson_4-4.kt
@@ -1,11 +1,13 @@
 package org.example.lesson_4
 
-fun main(){
+fun main() {
     val trainingDay = 5
-    println("""
+    println(
+        """
         |Упражнения для рук:   ${trainingDay % 2 == 1}
         |Упражнения для ног:   ${trainingDay % 2 == 0}
         |Упражнения для спины: ${trainingDay % 2 == 0}
         |Упражнения для пресса:${trainingDay % 2 == 1}
-    """.trimMargin())
+    """.trimMargin()
+    )
 }
