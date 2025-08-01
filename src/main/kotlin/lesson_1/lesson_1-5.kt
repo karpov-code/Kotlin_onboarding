d0a79aab1cd48f53a1668014132bb26dbc9fec5f
Карпов Андрей <andreyka527@yandex.ru>
2025-08-01 23:50:00 +0300

    KS-3-1-formatting 2.0

--- src/main/kotlin/lesson_1/lesson_1-5.kt
+++ src/main/kotlin/lesson_1/lesson_1-5.kt
@@ -5,12 +5,11 @@ const val TRANSLATION_IN_HOURS_AND_MINUTES = 60
 fun main() {
     val totalNumberOfSeconds: Short = 6480
     val totalNumberOfMinutes: Int = totalNumberOfSeconds / TRANSLATION_IN_HOURS_AND_MINUTES
+    val seconds = totalNumberOfSeconds % TRANSLATION_IN_HOURS_AND_MINUTES
+    val hours = totalNumberOfMinutes / TRANSLATION_IN_HOURS_AND_MINUTES
+    val minutes = totalNumberOfMinutes % TRANSLATION_IN_HOURS_AND_MINUTES
 
-    val seconds = String.format("%02d", totalNumberOfSeconds % TRANSLATION_IN_HOURS_AND_MINUTES)
-    val hours = String.format("%02d", totalNumberOfMinutes / TRANSLATION_IN_HOURS_AND_MINUTES)
-    val minutes = String.format("%02d", totalNumberOfMinutes % TRANSLATION_IN_HOURS_AND_MINUTES)
-
-    println("$hours:$minutes:$seconds")
+    println("%02d:%02d:%02d".format(hours, minutes, seconds))
 }
 
 
